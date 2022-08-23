dd5cbf94f6710a96ffc565cc022e47507ed72f6e
lukaszkus <kusiu@vp.pl>
2022-08-23 23:18:22 +0200

    modal - added html structure and styles, implemented functionality in js

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,26 @@
+//page title
 let firstName = "Łukasz";
 let lastName = "Kuś";
 
 document.title = `${firstName} ${lastName} - Recruitment task`;
+
+//modal
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const modalOpen = document.querySelector(".btn--modal-open");
+const modalClose = document.querySelector(".modal__close-btn");
+
+modalOpen.addEventListener("click", () => {
+  modal.classList.add("active");
+  overlay.classList.add("active");
+});
+
+modalClose.addEventListener("click", () => {
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
+});
+
+overlay.addEventListener("click", () => {
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
+});
