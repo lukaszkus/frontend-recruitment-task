b38555c0cda3e15a2198025a2df6d9fc6d970783
lukaszkus <kusiu@vp.pl>
2022-08-24 15:00:07 +0200

    updated modal, added reset button and js functionality

--- src/js/main.js
+++ src/js/main.js
@@ -7,20 +7,56 @@ document.title = `${firstName} ${lastName} - Recruitment task`;
 //modal
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
-const modalOpen = document.querySelector(".btn--modal-open");
-const modalClose = document.querySelector(".modal__close-btn");
+const openBtn = document.querySelector(".btn--modal-open");
+const closeBtn = document.querySelector(".modal__close-btn");
 
-modalOpen.addEventListener("click", () => {
+const openModal = () => {
   modal.classList.add("active");
   overlay.classList.add("active");
-});
+  clickCounter();
+  addResetBtn();
+};
 
-modalClose.addEventListener("click", () => {
+const closeModal = () => {
   modal.classList.remove("active");
   overlay.classList.remove("active");
-});
+};
 
-overlay.addEventListener("click", () => {
-  modal.classList.remove("active");
-  overlay.classList.remove("active");
-});
+openBtn.addEventListener("click", openModal);
+closeBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+//click counter
+const showClicks = document.querySelector(".modal__counter");
+let message = document.querySelector(".modal__message");
+
+const clickCounter = () => {
+  if (typeof Storage !== "undefined") {
+    if (localStorage.clickcount) {
+      localStorage.clickcount = Number(localStorage.clickcount) + 1;
+      console.log(localStorage.clickcount);
+    } else {
+      localStorage.clickcount = 1;
+    }
+    showClicks.innerHTML = localStorage.clickcount + " times";
+  } else {
+    message.innerHTML = "Sorry, your browser does not support web storage...";
+  }
+};
+
+const modalBody = document.querySelector(".modal__body");
+const resetBtn = document.createElement("button");
+resetBtn.classList.add("btn", "btn--counter-reset");
+resetBtn.textContent = "Reset counter";
+
+const addResetBtn = () => {
+  localStorage.clickcount >= 5 ? modalBody.appendChild(resetBtn) : null;
+  console.log("reset button added");
+  return resetBtn.addEventListener("click", resetCounter);
+};
+
+const resetCounter = () => {
+  localStorage.clickcount = 0;
+  console.log(`reset - counter is now ${localStorage.clickcount}`);
+  return (showClicks.innerHTML = localStorage.clickcount + " times");
+};
