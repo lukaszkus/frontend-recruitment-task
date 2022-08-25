3b30c03c9af76c5d06773da47c4d2353f4620edd
lukaszkus <kusiu@vp.pl>
2022-08-25 13:24:37 +0200

    refactoring modal, added all functionalities

--- src/js/main.js
+++ src/js/main.js
@@ -9,8 +9,9 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const openBtn = document.querySelector(".btn--modal-open");
 const closeBtn = document.querySelector(".modal__close-btn");
-const showCounter = document.querySelector(".modal__counter");
+const showClicks = document.querySelector(".modal__counter");
 const resetBtn = document.querySelector(".btn--reset");
+const showMsg = document.querySelector(".modal__message");
 
 const openModal = () => {
   modal.classList.add("active");
@@ -35,21 +36,38 @@ const clickCounter = () => {
   } else {
     sessionStorage.clickcount = 1;
   }
-  showCounter.innerHTML = sessionStorage.clickcount + " times";
+  showClicks.innerHTML = sessionStorage.clickcount + " times";
+};
+
+const message = {
+  error:
+    "You have reached the maximum capacity of our counter. Please click the reset button and start again.",
+  succes: "Thank you. The counter has been reset.",
 };
 
 const showResetBtn = () => {
   if (sessionStorage.clickcount >= 5) {
     resetBtn.classList.remove("disabled");
+    openBtn.classList.add("disabled-events");
+    overlay.classList.add("disabled-events");
+    closeBtn.classList.add("disabled-events", "opacity");
+    showMsg.innerHTML = message.error;
   } else {
     resetBtn.classList.add("disabled");
+    overlay.classList.remove("modal-disabled");
+    closeBtn.classList.remove("modal-disabled");
+    showMsg.innerHTML = "";
   }
 };
 
 const resetCounter = () => {
   sessionStorage.clickcount = 0;
-  showCounter.innerHTML = sessionStorage.clickcount + " times";
+  showClicks.innerHTML = sessionStorage.clickcount + " times";
+  showMsg.innerHTML = message.succes;
   resetBtn.classList.add("disabled");
+  overlay.classList.remove("disabled-events");
+  closeBtn.classList.remove("disabled-events", "opacity");
+  openBtn.classList.remove("disabled-events");
 };
 
 resetBtn.addEventListener("click", resetCounter);
